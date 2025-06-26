b5c0e69daa46eb9757ec789ced263b3605b1c690
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-06-27 00:32:06 +0600

    landing page complete

--- src/pages/BrowseTasks.jsx
+++ src/pages/BrowseTasks.jsx
@@ -7,25 +7,30 @@ const BrowseTasks = () => {
   return (
     <div className="container mx-auto px-3 lg:px-0 pt-30 pb-10">
       <h2 className="text-3xl font-bold text-center mb-8">Browse All Tasks</h2>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
         {allTasks.map((task) => (
           <div
             key={task._id}
-            className=" shadow-md rounded-xl p-5 border border-gray-200 hover:shadow-lg transition duration-300"
+            className="flex flex-col justify-between h-full shadow-md rounded-xl p-5 border border-gray-200 hover:shadow-lg transition duration-300"
           >
-            <h3 className="text-xl font-semibold mb-2">{task.title}</h3>
-            <p className="text-sm mb-1">
-              <strong>Category:</strong> {task.category}
-            </p>
-            <p className="text-sm  mb-1">
-              <strong>Budget:</strong> ${task.budget}
-            </p>
-            <p className="text-sm  mb-1">
-              <strong>Deadline:</strong> {task.deadline} days
-            </p>
-            <p className="text-sm  mb-3">{task.description?.slice(0, 80)}...</p>
+            <div>
+              <h3 className="text-xl font-semibold mb-2">{task.title}</h3>
+              <p className="text-sm mb-1">
+                <strong>Category:</strong> {task.category}
+              </p>
+              <p className="text-sm mb-1">
+                <strong>Budget:</strong> ${task.budget}
+              </p>
+              <p className="text-sm mb-1">
+                <strong>Deadline:</strong> {task.deadline} days
+              </p>
+              <p className="text-sm mb-3">
+                {task.description?.slice(0, 80)}...
+              </p>
+            </div>
 
-            <Link to={`/task/${task._id}`}>
+            {/* button always at bottom */}
+            <Link to={`/task/${task._id}`} className="mt-4">
               <button className="btn btn-sm btn-primary w-full">
                 See Details
               </button>
